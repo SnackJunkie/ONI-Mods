36d1c5c463d9e17bfad2aefdd7e72aafbe6003a3
SnackJunkie <130875027+SnackJunkie@users.noreply.github.com>
2023-08-29 17:15:41 -0400

    1

---
+++ src/Pipe Flow Overlay/Pipe Flow Overlay/PipeFlowOverlayIconController.cs
@@ -0,0 +1,36 @@
+﻿using PeterHan.PLib.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PipeFlowOverlay
+{
+    internal class PipeFlowOverlayIconController : KMonoBehaviour
+    {
+        private Image _image;
+
+        internal bool IconIsDirty { get; set; }
+        internal ConduitType ConduitType { get; set; }
+        internal ConduitFlow.SOAInfo SOAInfo { get; set; }
+        internal int Cell { get; set; }
+
+        protected override void OnPrefabInit()
+        {
+            _image = gameObject.AddOrGet<Image>();
+        }
+
+        private void Update()
+        {
+            if (IconIsDirty)
+            {
+                IconIsDirty = false;
+                ConduitFlow.FlowDirections flowDirections = SOAInfo.GetPermittedFlowDirections(Cell);
+                Debug.Log(flowDirections.ToString());
+            }
+        }
+    }
+}
--- src/Pipe Flow Overlay/Pipe Flow Overlay/Properties/Resources.Designer.cs
+++ src/Pipe Flow Overlay/Pipe Flow Overlay/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Pipe_Flow_Overlay.Properties {
+namespace PipeFlowOverlay.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Pipe_Flow_Overlay.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pipe_Flow_Overlay.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PipeFlowOverlay.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
